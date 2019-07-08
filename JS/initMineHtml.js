b4088dc6c11a59244608c4fb90bc56c01624877b
liuslin28 <liuslin28@163.com>
2019-07-08 17:33:10 +0800

    添加 地图编辑功能

--- JS/initMineHtml.js
+++ JS/initMineHtml.js
@@ -1,6 +1,6 @@
 /*------------------------------*/
 // 图层显示切换
-$('input').click(function (e) {
+$('.layerWrapper-check-input').click(function (e) {
     let inputTarget = e.target;
     let layerName = inputTarget.name;
     let checkValue = inputTarget.checked;
@@ -25,13 +25,19 @@ $('.layerWrapper-angle').click(function () {
 /*------------------------------*/
 // 交通信息列表显示切换
 $('#info-click').click(function () {
-    $('.dataWrapper').slideToggle()
+    $('#simulate-dataWrapper').hide();
+    $('#info-dataWrapper').slideToggle();
+});
+
+// 交通仿真列表显示切换
+$('#simulate-click').click(function () {
+    $('#info-dataWrapper').hide();
+    $('#simulate-dataWrapper').slideToggle();
 });
 
 $('.dataWrapper-menu-li-a').click(function (e) {
     let menuList = e.target;
     let menuListName = menuList.name;
-    // 获取当前列表名称
     $(".dataWrapper-list-menu").each(function(index,domEle) {
         $(domEle).hide();
     });
@@ -40,10 +46,20 @@ $('.dataWrapper-menu-li-a').click(function (e) {
 
 /*------------------------------*/
 // 列表切换样式
+// 导航栏列表
+// 有bug待改
 $('.navWrapper-nav-li').click(function () {
-    $(this).toggleClass("navWrapper-nav-li-active");
+    // let _this = this;
+    let navId = this.id;
+    if (navId === 'layer-click') {
+        $('#layer-click').toggleClass("navWrapper-nav-li-active");
+    } else {
+        $('#layer-click').siblings('li').removeClass('navWrapper-nav-li-active');
+        $('#'+navId).addClass('navWrapper-nav-li-active');
+    }
+    console.log(this.id);
 });
-
+// 侧边栏列表
 $('.dataWrapper-list-menu-li').click(function () {
     $(this).siblings('li').removeClass('dataWrapper-list-menu-li-active');
     $(this).addClass('dataWrapper-list-menu-li-active');
