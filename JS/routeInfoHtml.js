1fed7895c2f64c3297b27a777cad764316b67916
liuslin28 <liuslin28@163.com>
2019-08-09 15:02:32 +0800

    修改 优化线路信息展示栏

--- JS/routeInfoHtml.js
+++ JS/routeInfoHtml.js
@@ -10,15 +10,29 @@ function routeInfoHtml(data) {
     let nonLinearIndex = nonLinear(data);
     // 站间距最大最小阈值
     let stationDistanceIndex = stationDistance(data,maxIndex,minIndex);
+    // 途径公交专用道长度
+    let buslaneLength = 4.68;
 
-    $('.routeInfoWrapper-title').text(dataProperty.lineName + "路公交车");
+    $('.routeInfoWrapper-title').empty().text(dataProperty.lineName + "路公交车");
     basicHtml = "<span class='routeInfoWrapper-title-name'>" + dataProperty.initStation + "---" + dataProperty.finalStation + "</span>" + "<span>" + dataProperty.stationNum + "站" +  "</span>" +  "<span>" + dataProperty.lineLength + "km" +  "</span>";
-    $('.routeInfoWrapper-basic').append(basicHtml);
+    $('.routeInfoWrapper-basic').empty().append(basicHtml);
 
     $('#index-distance').text(distanceIndex);
     $('#index-nonLinear').text(nonLinearIndex);
     $('#index-maxDis').text(stationDistanceIndex.maxRatio);
     $('#index-minDis').text(stationDistanceIndex.minRatio);
+    $('#index-laneLength').text(buslaneLength);
 
     $('.routeInfoWrapper').show();
 }
+
+// 关闭按钮，移除线路信息框及线路图层
+$('.routeInfoWrapper-button').click(function () {
+   $('.routeInfoWrapper').hide();
+    closeLayer();
+    layerVisibilityToggle("stationLayerB", 'visible');
+    layerVisibilityToggle("stationLayerC", 'visible');
+    layerVisibilityToggle("stationLayerD", 'visible');
+    layerVisibilityToggle("terminalLayer", 'visible');
+    changeStationLayer('true');
+});
